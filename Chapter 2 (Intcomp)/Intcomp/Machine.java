e3e4334e50524273fa6e2477417899963c3fe0b0
Gustav Metnik-Beck <gustav.mb@outlook.com>
2023-01-03 14:46:27 +0100

    setup: Chapter 2

--- Chapter 2 (Intcomp)/Intcomp/Machine.java
+++ Chapter 2 (Intcomp)/Intcomp/Machine.java
@@ -17,48 +17,58 @@
    not rely on object-orientation or garbage collection.  */
 
 class Machine {
-  final static int 
-    SCST = 0, SVAR = 1, SADD = 2, SSUB = 3, SMUL = 4, SPOP = 5, SSWAP = 6;
-  
-  public static void main(String[] args) {
-    final int[] rpn1 = { SCST, 17, SVAR, 0, SVAR, 1, SADD, SSWAP, SPOP };
-    System.out.println(seval(rpn1));
-    final int[] rpn2 = { SCST, 17, SCST, 22, SCST, 100, SVAR, 1, SMUL, 
-			 SSWAP, SPOP, SVAR, 1, SADD, SSWAP, SPOP };
-    System.out.println(seval(rpn2));
-  }
+      final static int SCST = 0, SVAR = 1, SADD = 2, SSUB = 3, SMUL = 4, SPOP = 5, SSWAP = 6;
 
-  static int seval(int[] code) {
-    int[] stack = new int[1000];	// evaluation and env stack
-    int sp = -1;			// pointer to current stack top
-
-    int pc = 0;				// program counter
-    int instr;				// current instruction
-
-    while (pc < code.length) 
-      switch (instr = code[pc++]) {
-      case SCST:
-	stack[sp+1] = code[pc++]; sp++; break;
-      case SVAR:
-	stack[sp+1] = stack[sp-code[pc++]]; sp++; break;
-      case SADD: 
-	stack[sp-1] = stack[sp-1] + stack[sp]; sp--; break;
-      case SSUB: 
-	stack[sp-1] = stack[sp-1] - stack[sp]; sp--; break;
-      case SMUL: 
-	stack[sp-1] = stack[sp-1] * stack[sp]; sp--; break;
-      case SPOP: 
-	sp--; break;
-      case SSWAP: 
-	{ int tmp     = stack[sp]; 
-	  stack[sp]   = stack[sp-1]; 
-	  stack[sp-1] = tmp;
-	  break;
-	}
-      default:			
-	throw new RuntimeException("Illegal instruction " + instr 
-				   + " at address " + (pc-1));
+      public static void main(String[] args) {
+            final int[] rpn1 = { SCST, 17, SVAR, 0, SVAR, 1, SADD, SSWAP, SPOP };
+            System.out.println(seval(rpn1));
+            final int[] rpn2 = { SCST, 17, SCST, 22, SCST, 100, SVAR, 1, SMUL,
+                        SSWAP, SPOP, SVAR, 1, SADD, SSWAP, SPOP };
+            System.out.println(seval(rpn2));
       }
-    return stack[sp];      
-  }
+
+      static int seval(int[] code) {
+            int[] stack = new int[1000]; // evaluation and env stack
+            int sp = -1; // pointer to current stack top
+
+            int pc = 0; // program counter
+            int instr; // current instruction
+
+            while (pc < code.length)
+                  switch (instr = code[pc++]) {
+                        case SCST:
+                              stack[sp + 1] = code[pc++];
+                              sp++;
+                              break;
+                        case SVAR:
+                              stack[sp + 1] = stack[sp - code[pc++]];
+                              sp++;
+                              break;
+                        case SADD:
+                              stack[sp - 1] = stack[sp - 1] + stack[sp];
+                              sp--;
+                              break;
+                        case SSUB:
+                              stack[sp - 1] = stack[sp - 1] - stack[sp];
+                              sp--;
+                              break;
+                        case SMUL:
+                              stack[sp - 1] = stack[sp - 1] * stack[sp];
+                              sp--;
+                              break;
+                        case SPOP:
+                              sp--;
+                              break;
+                        case SSWAP: {
+                              int tmp = stack[sp];
+                              stack[sp] = stack[sp - 1];
+                              stack[sp - 1] = tmp;
+                              break;
+                        }
+                        default:
+                              throw new RuntimeException("Illegal instruction " + instr
+                                          + " at address " + (pc - 1));
+                  }
+            return stack[sp];
+      }
 }
