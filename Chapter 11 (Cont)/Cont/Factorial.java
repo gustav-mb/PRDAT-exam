0323bfe09a3f46ce3ef2d94efbe238113dd968c2
Gustav Metnik-Beck <gustav.mb@outlook.com>
2023-01-03 20:24:34 +0100

    setup: Chapter 11

--- Chapter 11 (Cont)/Cont/Factorial.java
+++ Chapter 11 (Cont)/Cont/Factorial.java
@@ -14,26 +14,26 @@ interface Cont {
 class Factorial {
   public static void main(String[] args) {
     int n = Integer.parseInt(args[0]);
-    System.out.println(facc(n, 
-                            new Cont() {
-                                public int k(int v) {
-                                  return v;
-                                }
-                              }));
+    System.out.println(facc(n,
+        new Cont() {
+          public int k(int v) {
+            return v;
+          }
+        }));
   }
 
-  // A continuation-passing version of factorial.  This is a straight
+  // A continuation-passing version of factorial. This is a straight
   // translation of the functional (F#) version:
 
   static int facc(final int n, final Cont cont) {
-    if (n == 0) 
+    if (n == 0)
       return cont.k(1);
     else
-      return facc(n-1, 
-                  new Cont() {
-                      public int k(int v) {
-                        return cont.k(n * v);
-                      }
-                    });
+      return facc(n - 1,
+          new Cont() {
+            public int k(int v) {
+              return cont.k(n * v);
+            }
+          });
   }
 }
---
+++ Chapter 11 (Cont)/run.ps1
@@ -0,0 +1,13 @@
+param([bool]$compile=$false)
+
+if ($compile) {
+    Write-Host "Compiling Java files..."
+    javac .\Cont\Factorial.java
+    Write-Host "Done."
+
+    Write-Host "Compiling testlongjmp.c"
+    gcc -Wall -g .\Cont\testlongjmp.c -o .\Cont\testlongjmp.exe 
+    Write-Host "Done."
+} else {
+    dotnet fsi .\Cont\Contfun.fs .\Cont\Contimp.fs .\Cont\Icon.fs
+}
