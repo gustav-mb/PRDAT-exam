c37d0f8000850363fb1554498adcd65977e7373b
Gustav Metnik-Beck <gustav.mb@outlook.com>
2023-01-03 15:12:36 +0100

    setup: Chapter 5 & 6

---
+++ Chapter 5 & 6 (HigherFun)/run.ps1
@@ -0,0 +1,9 @@
+param([bool]$compile=$false)
+
+if ($compile) {
+    Write-Host "Compiling Java files..."
+    javac .\Fun\LinkedList.java
+    Write-Host "Done."
+} else {
+    dotnet fsi -r .\bin\Debug\net7.0\FsLexYacc.Runtime.dll .\Fun\Absyn.fs .\Fun\FunPar.fsi .\Fun\FunPar.fs .\Fun\FunLex.fs .\Fun\Parse.fs .\Fun\Fun.fs .\Fun\ParseAndRun.fs .\Fun\HigherFun.fs .\Fun\ParseAndRunHigher.fs .\Fun\TypeInference.fs .\Fun\ParseAndType.fs
+}
